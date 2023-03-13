3f87b50db62825f08d46718b4bf26d4109226e13
gnnpls <n.giannopoulos@motivar.io>
2023-03-13 14:41:21 +0200

    fixes

--- lib/composer/autoload_real.php
+++ lib/composer/autoload_real.php
@@ -29,7 +29,7 @@ class ComposerAutoloaderInitfb3118450273b47d95f6da240d881eb0
         require __DIR__ . '/autoload_static.php';
         call_user_func(\Composer\Autoload\ComposerStaticInitfb3118450273b47d95f6da240d881eb0::getInitializer($loader));
 
-        $loader->setApcuPrefix('JsnN1sVXSwMy3q38Blx8T');
+        $loader->setApcuPrefix('+PweeVxdzZ3nRPuuKfn9M');
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitfb3118450273b47d95f6da240d881eb0::$files;
