4e328e8e3ef382908a71208529b5f3b3b6448914
gnnpls <n.giannopoulos@motivar.io>
2023-12-29 09:18:37 +0200

    fix for loading images

--- includes/classes/gallery-meta-box/js/gallery-meta-box.js
+++ includes/classes/gallery-meta-box/js/gallery-meta-box.js
@@ -1,4 +1,4 @@
-( function( $ ) {
+(function ($) {
 	"use strict";
 	var gallery = window.truongwpGallery = {
 
@@ -10,18 +10,18 @@
 
 		idsInput: '#truongwp-gallery-ids',
 
-		getIds: function() {
+		getIds: function () {
 			var _this = gallery,
-				ids = $( _this.idsInput ).val();
+				ids = $(_this.idsInput).val();
 
 			return ids ? ids.split(',') : [];
 		},
 
-		frame: function() {
-			if ( this._frame )
+		frame: function () {
+			if (this._frame)
 				return this._frame;
 
-			this._frame = wp.media( {
+			this._frame = wp.media({
 				title: wp.media.view.l10n.addToGalleryTitle,
 				button: {
 					text: wp.media.view.l10n.addToGallery
@@ -30,69 +30,71 @@
 				library: {
 					type: 'image'
 				}
-			} );
-			this._frame.on( 'open', this.open );
+			});
+			this._frame.on('open', this.open);
 
-			this._frame.state( 'library' ).on( 'select', this.select );
+			this._frame.state('library').on('select', this.select);
 
 			return this._frame;
 		},
 
-		open: function() {
+		open: function () {
+
 			var _this = gallery,
 				ids = _this.getIds(),
 				library = _this.frame().state().get('library'),
 				selection = _this.frame().state().get('selection');
 
+			wp.media.query({ post__in: ids }).more().then(function () {
+				_.each(ids, function (id) {
+					var attachment = wp.media.attachment(id);
+					library.unshift(attachment ? [attachment] : []);
+					selection.add(attachment);
+				});
+			});
 
-			for ( var i in ids ) {
-				var attachment = wp.media.attachment( ids[i] );
-				attachment.fetch();
-				selection.add( attachment );
-				library.add( attachment ? [ attachment ] : [] );
-			}
 		},
 
-		select: function() {
+		select: function () {
 			var _this = gallery,
-				selection = this.get( 'selection' );
+				selection = this.get('selection');
 
-			$( _this.imageContainer ).html('');
-			selection.map( _this.showAttachments );
+			$(_this.imageContainer).html('');
+			selection.map(_this.showAttachments);
 		},
 
-		showAttachments: function( attachment ) {
+		showAttachments: function (attachment) {
 			var _this = gallery, url, tpl;
 
 			url = attachment.attributes.sizes.thumbnail ? attachment.attributes.sizes.thumbnail.url : attachment.attributes.url;
 
-			tpl = _this.imageTpl( { id: attachment.id, url: url } );
+			tpl = _this.imageTpl({ id: attachment.id, url: url });
 
-			$( _this.imageContainer ).append( tpl );
+			$(_this.imageContainer).append(tpl);
 		},
 
-		init: function() {
+		init: function () {
 			var _this = this;
 
-			$( this.buttonId ).on( 'click', function( ev ) {
+			$(this.buttonId).on('click', function (ev) {
 				ev.preventDefault();
 
 				_this.frame().open();
 			});
 
-			$( this.imageContainer ).on( 'click', '.gallery-remove', function( ev ) {
+			$(this.imageContainer).on('click', '.gallery-remove', function (ev) {
 				ev.preventDefault();
 
 				$(this).closest('.gallery-item').remove();
 			});
 
-			$( this.imageContainer ).sortable({
+			$(this.imageContainer).sortable({
 				placeholder: 'gallery-placeholder'
 			});
 		}
 	};
 
-	$(document).ready( function() {
+	$(document).ready(function () {
 		gallery.init();
 	});
-})( jQuery );
+})(jQuery);
