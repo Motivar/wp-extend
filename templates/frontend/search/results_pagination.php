d94b7d96a5f8b3689eb9e39bc6996472f8137e8a
gnnpls <n.giannopoulos@motivar.io>
2024-04-19 12:42:54 +0300

    fixes

--- templates/frontend/search/results_pagination.php
+++ templates/frontend/search/results_pagination.php
@@ -4,32 +4,29 @@
  * The Template for display search results
  */
 if (!defined('ABSPATH')) {
- exit; // Exit if accessed directly
+  exit; // Exit if accessed directly
 }
 
 global $ewp_search_query;
-  global $ewp_config;
-  $current_page = max(1, isset($ewp_search_query->query_vars['paged']) ? $ewp_search_query->query_vars['paged'] : 1);
+global $ewp_config;
+$current_page = max(1, isset($ewp_search_query->query_vars['paged']) ? $ewp_search_query->query_vars['paged'] : 1);
 ?>
-<div class="ewp-search-pagination">
-
- <?php
-  switch ($ewp_config['load_type']) {
-    case 'button':
-      if ($current_page < $ewp_search_query->max_num_pages) {
-        echo '<button class="ewp-load-more" data-page="' . $current_page . '" data-max-pages="' . $ewp_search_query->max_num_pages . '">' . __($ewp_config['load_type_button'], 'extend-wp') . '</button>';
-      }
-      break;
-    default:
- echo paginate_links(array(
-  'format' => 'page/%#%/',
-        'current' => $current_page,
-  'total' => $ewp_search_query->max_num_pages,
-  'prev_text' => __('« prev', 'extend-wp'),
-  'next_text' => __('next »', 'extend-wp'),
-  'show_all' => true
- ));
-      break;
-  }
- ?>
-</div>
+<div class="ewp-search-pagination"><?php
+                                    switch ($ewp_config['load_type']) {
+                                      case 'button':
+                                        if ($current_page < $ewp_search_query->max_num_pages) {
+                                          echo '<button class="ewp-load-more" data-page="' . $current_page . '" data-max-pages="' . $ewp_search_query->max_num_pages . '">' . __($ewp_config['load_type_button'], 'extend-wp') . '</button>';
+                                        }
+                                        break;
+                                      default:
+                                        echo paginate_links(array(
+                                          'format' => 'page/%#%/',
+                                          'current' => $current_page,
+                                          'total' => $ewp_search_query->max_num_pages,
+                                          'prev_text' => __('« prev', 'extend-wp'),
+                                          'next_text' => __('next »', 'extend-wp'),
+                                          'show_all' => true
+                                        ));
+                                        break;
+                                    }
+                                    ?></div>
