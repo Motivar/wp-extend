99f4617e3ccbbb3bd4cb310d72367f9b07545151
gnnpls <n.giannopoulos@motivar.io>
2024-12-11 11:11:45 +0200

    add user role to access the page

--- includes/classes/awm-content-db-api/class-defaults.php
+++ includes/classes/awm-content-db-api/class-defaults.php
@@ -11,16 +11,45 @@ class Extend_WP_Default_Content
 
   public function __construct()
   {
+
+
+
     add_filter('awm_add_options_boxes_filter', [$this, 'admin_menu']);
   }
   public function admin_menu($options)
   {
+
+    $allowed_users = get_option('ewp_user_access', array());
+    $allowed_users[] = 'administrator';
+    /*check if current user can access the page based on the user role get first the user role and then check if in array*/
+    $user = wp_get_current_user();
+    $user_role = $user->roles[0];
+    if (!in_array($user_role, $allowed_users)) {
+      return $options;
+    }
+  
     $options['extend-wp'] = array(
       'title' => apply_filters('ewp_whitelabel_filter', __('Extend WP', 'extend-wp')),
       'parent' => false,
       'cap' => 'manage_options',
+      'icon' => 'dashicons-admin-generic',
+
+      'library' => $this->admin_fields()
     );
     return $options;
+  }
+
+  public function admin_fields()
+  {
+    return apply_filters('ewp_admin_fields_filter', array(
+      'ewp_user_access' => array(
+        'case' => 'user_roles',
+        'exclude' => array('administrator'),
+        'attributes' => array('multiple' => true),
+        'label' => __('Select user roles with access to the plugin', 'extend-wp'),
+      ),
+
+    ));
   }
 }
 
