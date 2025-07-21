9cc0023867436dc238b54f09310c5be03e4ff9fe
gnnpls <n.giannopoulos@motivar.io>
2025-07-21 10:02:54 +0300

    fix: add descriptive message to debug die() statement

--- includes/functions/main.php
+++ includes/functions/main.php
@@ -351,7 +351,7 @@ if (!function_exists('ewp_debug')) {
         echo htmlspecialchars(print_r($data, true), ENT_QUOTES, 'UTF-8');
         echo '</pre>';
         if ($exit) {
-            die();
+            die('end-of-debug');
         }
     }
 }
