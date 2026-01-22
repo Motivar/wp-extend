46cf575e08e230a2e82a6e1c9672097a2ca9b85f
gnnpls <n.giannopoulos@motivar.io>
2026-01-22 12:10:46 +0200

    chore: update Composer autoloader APCu prefix

--- lib/composer/autoload_real.php
+++ lib/composer/autoload_real.php
@@ -29,7 +29,7 @@ class ComposerAutoloaderInitfb3118450273b47d95f6da240d881eb0
         require __DIR__ . '/autoload_static.php';
         call_user_func(\Composer\Autoload\ComposerStaticInitfb3118450273b47d95f6da240d881eb0::getInitializer($loader));
 
-        $loader->setApcuPrefix('8a75d59bdc24872350ce');
+        $loader->setApcuPrefix('3f3752ebf6114f3f3b64');
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitfb3118450273b47d95f6da240d881eb0::$files;
